e3589367ef6e9a1bf6cfbbdf6e1be33aae68baf0
semanurbas <40912265+semanurbas@users.noreply.github.com>
2021-09-30 15:46:53 +0300

    Update WebSecurityConfig.java

--- myUserServiceProject/src/main/java/springBoot/myUserServiceProject/security/WebSecurityConfig.java
+++ myUserServiceProject/src/main/java/springBoot/myUserServiceProject/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
